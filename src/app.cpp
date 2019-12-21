e1b7596c7001949748d13d50e5daacdd9fc2aa5f
lenkite <dev@elankath.com>
2019-12-21 18:44:48 +0530

    added experimental source location

--- src/app.cpp
+++ src/app.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include <string>
 #include <set>
+#include <experimental/source_location>
 #include "basic/app.h"
+
+void log(std::string_view message,
+         const std::experimental::source_location& location = std::experimental::source_location::current()) {
+    std::cout << "info:"
+              << location.file_name() << ":"
+              << location.line() << " "
+              << message << '\n';
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     auto p = person{.name ="bingo", .age = 23};
-    std::cout << p.name << std::endl;
+    std::cout << "P.name = " << p.name << std::endl;
+    log("Logging Hello world!");
     return 0;
 }
 
